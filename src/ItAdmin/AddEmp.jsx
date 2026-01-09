ec987d57ef3942c129fde89dab26cc7e54f3b4a7
harsha-gundre2806 <harshagundre2806@gmail.com>
2026-01-09 11:52:26 +0530

    add emp added with notification

--- src/ItAdmin/AddEmp.jsx
+++ src/ItAdmin/AddEmp.jsx
@@ -1,1 +1,61 @@
 // adding employee
+import React, { useState } from 'react';
+import Notification from '../pages/Notification';
+import './AddEmp.css'
+
+
+export default function AddEmp() {
+  const [role, setRole] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [notify, setNotify] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!role) {
+      setNotify('Please select a role');``
+      return;
+    }
+    setNotify(`${role} added successfully`);
+    setTimeout(() => setNotify(''), 5000);
+  };
+
+  return (
+    <>
+      {notify && <Notification message={notify} />}
+
+      <div className="container">
+        <form onSubmit={handleSubmit} className="form">
+        <select
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            className="input"
+            required >
+            <option value="">Select Role</option>
+            <option value="Hospital Admin">Hospital Admin</option>
+            <option value="ICU Manager">ICU Manager</option>
+            <option value="Ward Staff">Ward Staff</option>
+            <option value="Er Staff">Er Staff</option>
+        </select>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="input" />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="input" />
+          <button type="submit" className="button">
+            Add Employee
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,1 +1,29 @@
 // for notifications
+
+
+import React, { useEffect, useState } from 'react';
+import './Notification.css';
+
+export default function Notification(
+    { message = " " } ) 
+    {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!visible) return null;
+
+  return (
+    <div className="wrapper">
+      <div className="notification">
+        {message}
+      </div>
+    </div>
+  );
+}
